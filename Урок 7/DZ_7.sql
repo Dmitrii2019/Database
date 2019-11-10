c42d7824465aed2859c24b81195f8fdb76728032
Dmitrii2019 <51288023+Dmitrii2019@users.noreply.github.com>
2019-11-10 15:37:40 +0200

    Update DZ_7.sql

--- Урок 7/DZ_7.sql
+++ Урок 7/DZ_7.sql
@@ -1,4 +1,4 @@
-
+-- Составьте список пользователей users, которые осуществили хотя бы один заказ orders в интернет магазине.
 USE shop;
 
 SELECT name
@@ -8,7 +8,7 @@ ON users.id = orders.user_id
 GROUP BY orders.user_id
 ORDER BY name;
 
-
+-- Выведите список товаров products и разделов catalogs, который соответствует товару.
 
 SELECT products.id, products.name AS products_name, catalogs.name AS catalogs_name
 FROM catalogs
